dec6f5566bee8ac75a7d755d4a13fcb05d727fb7
Flynn <flynnigan.tan@gmail.com>
2017-07-29 00:43:29 -0500

    added bottom content and updated css

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+
+// 	// hide the bottom half of the index page until clicked
+
+// 	$("#bottom").hide();
+
+// 	// once anything with .btn is clicked, reveal the bottom
+// 	$(".btn").on("click", function(){
+
+// 		$("#bottom").fadeIn(3000).slideUp(400).show();
+// 	});
+// });
