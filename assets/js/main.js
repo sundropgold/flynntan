8bed6198697e5ac85bc093e2e4cfb9a10065862d
Flynn <flynnigan.tan@gmail.com>
2017-09-05 23:25:57 -0500

    added top button and back to home buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,52 @@ $(document).ready(function(){
 
 	// then fade in sequence
 	$('#sequence').fadeIn(1000);
+
+	// initially hide the bottom
+	$('#about-preview').hide();
+	$('#code-preview').hide();
+	$('#art-preview').hide();
+	$('#contact-preview').hide();
+
+	// show about preview
+	$("#abt-seq").on("click", function(){
+		
+		$('#sequence').hide();
+		$('#about-preview').fadeIn(2000);
+
+	});
+
+	// show code preview
+	$("#code-seq").on("click", function(){
+		
+		$('#sequence').hide();
+		$('#code-preview').fadeIn(2000);
+
+	});
+
+	// show art preview
+	$("#art-seq").on("click", function(){
+		
+		$('#sequence').hide();
+		$('#art-preview').fadeIn(2000);
+
+	});
+
+	// show contact preview
+	$("#con-seq").on("click", function(){
+		
+		$('#sequence').hide();
+		$('#contact-preview').fadeIn(2000);
+
+	});
+
+	// from previews, go back to home
+	$(document).on("click", ".back-home", function(){
+		$('#about-preview').fadeOut();
+		$('#code-preview').fadeOut();
+		$('#art-preview').fadeOut();
+		$('#contact-preview').fadeOut();
+
+		$('#sequence').fadeIn(1000);
+	});
 });
